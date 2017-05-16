dd550b84e1a495dd6e08630e6cf12e6f9c08df79
Artem Fedorov <artem.fedorov@blazemeter.com>
2017-05-16 10:45:06 +0300

    add temp test

---
+++ src/test/java/org/jmeterplugins/repository/JARSourceHTTPTest.java
@@ -0,0 +1,13 @@
+package org.jmeterplugins.repository;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JARSourceHTTPTest {
+
+    @Test
+    public void testInstallId() throws Exception {
+        assertEquals(System.getenv().toString(), 1, 3);
+    }
+}
