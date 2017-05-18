c512507a2489d97b81927bf50573b38b798d725b
Artem Fedorov <artem.fedorov@blazemeter.com>
2017-05-18 16:32:18 +0300

    add Library entity

---
+++ src/main/java/org/jmeterplugins/repository/Library.java
@@ -0,0 +1,98 @@
+package org.jmeterplugins.repository;
+
+import java.util.Comparator;
+
+public class Library {
+
+    protected String name;
+    protected String version;
+    protected String condition;
+    protected String link;
+
+    public static final Comparator<Library> versionComparator = new Comparator<Library>() {
+        @Override
+        public int compare(Library lib1, Library lib2) {
+            int code = conditionComparator.compare(lib1, lib2);
+            if (code != 0) {
+                return code;
+            }
+
+            final String[] versions1 = lib1.getVersion().split("[.]");
+            final String[] versions2 = lib2.getVersion().split("[.]");
+            int length = (versions1.length > versions2.length) ? versions2.length : versions1.length;
+
+            for (int i = 0; i < length; i++) {
+                code = Integer.compare(Integer.parseInt(versions1[i]), Integer.parseInt(versions2[i]));
+                if (code != 0) {
+                    break;
+                }
+            }
+
+            // if version1: 9.9 and version2: 9.9.1
+            if (code == 0 && versions1.length != versions2.length) {
+                code = Integer.compare(versions1.length, versions2.length);
+            }
+
+
+            return code;
+        }
+    };
+
+    public static final Comparator<Library> conditionComparator = new Comparator<Library>() {
+        // '==' have higher priority than '>='
+        @Override
+        public int compare(Library lib1, Library lib2) {
+            if ("==".equals(lib1.getCondition()) && ">=".equals(lib2.getCondition())) {
+                return 1;
+            } else if (">=".equals(lib1.getCondition()) && "==".equals(lib2.getCondition())) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+    };
+
+
+
+    public Library() {
+    }
+
+    public Library(String name, String condition, String version, String link) {
+        this.name = name;
+        this.condition = condition;
+        this.version = version;
+        this.link = link;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
---
+++ src/test/java/org/jmeterplugins/repository/LibraryTest.java
@@ -0,0 +1,40 @@
+package org.jmeterplugins.repository;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LibraryTest {
+    @Test
+    public void testFlow() throws Exception {
+        Library lib1 = new Library("name", "==", "9.9.9", "LINK");
+
+        Library lib2 = new Library();
+        lib2.setName("name2");
+        lib2.setCondition("==");
+        lib2.setVersion("9.9.8");
+        lib2.setLink("LINK2");
+
+        assertEquals("name2", lib2.getName());
+        assertEquals("==", lib2.getCondition());
+        assertEquals("9.9.8", lib2.getVersion());
+        assertEquals("LINK2", lib2.getLink());
+
+        assertEquals(1, Library.versionComparator.compare(lib1, lib2));
+
+        lib2.setVersion("9.9.10");
+        assertEquals(-1, Library.versionComparator.compare(lib1, lib2));
+
+        lib1.setVersion("9.9");
+        assertEquals(-1, Library.versionComparator.compare(lib1, lib2));
+
+        lib1.setVersion("1.1");
+        lib2.setVersion("1.2");
+        lib1.setCondition(">=");
+        assertEquals(-1, Library.versionComparator.compare(lib1, lib2));
+
+        lib2.setCondition(">=");
+        lib1.setCondition("==");
+        assertEquals(1, Library.versionComparator.compare(lib1, lib2));
+    }
+}
