8a9422fcc5a41f7f7b8cc14a67150087706ffca2
Artem Fedorov <artem.fedorov@blazemeter.com>
2017-07-31 18:16:35 +0300

    add test

---
+++ src/test/java/org/jmeterplugins/repository/plugins/PluginSuggesterTest.java
@@ -0,0 +1,32 @@
+package org.jmeterplugins.repository.plugins;
+
+import kg.apc.emulators.TestJMeterUtils;
+import org.apache.jmeter.util.JMeterUtils;
+import org.jmeterplugins.repository.PluginManager;
+import org.jmeterplugins.repository.PluginManagerTest;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.net.URL;
+
+
+public class PluginSuggesterTest {
+    @BeforeClass
+    public static void setup() {
+        TestJMeterUtils.createJmeterEnv();
+    }
+
+
+    @Test
+    public void testFlow() throws Throwable {
+        URL repo = PluginManagerTest.class.getResource("/suggest.json");
+        URL testPlan = PluginManagerTest.class.getResource("/testplan.xml");
+
+        JMeterUtils.setProperty("jpgc.repo.address", repo.getPath());
+        PluginManager pmgr = PluginManager.getStaticManager();
+        pmgr.load();
+
+        PluginSuggester suggester = new PluginSuggester();
+        suggester.checkAndSuggest("Loading file : " + testPlan.getPath());
+    }
+}
