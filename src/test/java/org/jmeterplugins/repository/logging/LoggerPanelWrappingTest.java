b9011278e33b6b8a17e56f21319e2443fc59a355
Artem Fedorov <artem.fedorov@blazemeter.com>
2017-08-01 11:37:35 +0300

    fixed travis

--- src/test/java/org/jmeterplugins/repository/logging/LoggerPanelWrappingTest.java
+++ src/test/java/org/jmeterplugins/repository/logging/LoggerPanelWrappingTest.java
@@ -1,23 +1,33 @@
 package org.jmeterplugins.repository.logging;
 
+import kg.apc.emulators.TestJMeterUtils;
 import org.apache.log.LogEvent;
 import org.jmeterplugins.repository.PluginManager;
 import org.jmeterplugins.repository.plugins.PluginSuggester;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.awt.*;
 
 import static org.junit.Assert.*;
 
 public class LoggerPanelWrappingTest {
+    @BeforeClass
+    public static void setup() {
+        TestJMeterUtils.createJmeterEnv();
+    }
 
     @Test
     public void testFlow() throws Exception {
-        LoggerPanelWrapping wrapping = new LoggerPanelWrapping(PluginManager.getStaticManager());
-        PluginSuggester suggester = new PluginSuggester(PluginManager.getStaticManager());
-        wrapping.setSuggester(suggester);
-        assertEquals(suggester, wrapping.getSuggester());
-        LogEvent event = new LogEvent();
-        event.setCategory("SaveService");
-        event.setMessage("Save File");
-        wrapping.processEvent(event);
+        if (!GraphicsEnvironment.getLocalGraphicsEnvironment().isHeadlessInstance()) {
+            LoggerPanelWrapping wrapping = new LoggerPanelWrapping(PluginManager.getStaticManager());
+            PluginSuggester suggester = new PluginSuggester(PluginManager.getStaticManager());
+            wrapping.setSuggester(suggester);
+            assertEquals(suggester, wrapping.getSuggester());
+            LogEvent event = new LogEvent();
+            event.setCategory("SaveService");
+            event.setMessage("Save File");
+            wrapping.processEvent(event);
+        }
     }
 }
