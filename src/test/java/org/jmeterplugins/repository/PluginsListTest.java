1823fc7a8a5abdf8a1a4752f59fcd1b7d455f163
Philippe M <pmouawad@users.noreply.github.com>
2017-12-05 09:16:10 +0100

    Add Maven Information (#29)
    * Add Maven Information
    
    * Test for null candidate version
    
    * Add JUnit test

--- src/test/java/org/jmeterplugins/repository/PluginsListTest.java
+++ src/test/java/org/jmeterplugins/repository/PluginsListTest.java
@@ -1,31 +1,41 @@
 package org.jmeterplugins.repository;
 
-import net.sf.json.JSONObject;
-import net.sf.json.JsonConfig;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
-import javax.swing.*;
-import javax.swing.event.ChangeListener;
-import javax.swing.event.ListSelectionEvent;
-import java.awt.*;
+import java.awt.KeyboardFocusManager;
 import java.awt.event.KeyEvent;
 import java.io.File;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import javax.swing.JComboBox;
+import javax.swing.JList;
+import javax.swing.JTextField;
+import javax.swing.ListModel;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ListSelectionEvent;
+
+import org.junit.Test;
+
+import net.sf.json.JSONObject;
+import net.sf.json.JsonConfig;
 
 public class PluginsListTest {
 
+    private static final String EXPECTED_DESC_MAVEN = 
+            "Maven Information: groupId:<i>kg.apc</i>, artifactId:<i>jmeter-plugins-webdriver</i>, version:<i>0.3</i>";
+    private static final String EXPECTED_DESC_CHANGES = 
+            "What's new in version 0.3: fix verified exception1";
     @Test
     public void testFlow() throws Exception {
         String imgPath = "file:///" + new File(".").getAbsolutePath() + "/target/classes/org/jmeterplugins/logo.png";
         String str = "{\"id\": 0,  \"markerClass\": \"" + PluginsListTest.class.getName() + "\"," +
                 " \"screenshotUrl\": \"" + imgPath + "\", \"name\": 3, \"description\": 4, \"helpUrl\": 5, \"vendor\": 5, \"installerClass\": \"test\", " +
-                "\"versions\" : { \"0.1\" : { \"changes\": \"fix verified exception1\" }," +
-                "\"0.2\" : { \"changes\": \"fix verified exception1\" }," +
-                "\"0.3\" : { \"changes\": \"fix verified exception1\" } }}";
+                "\"versions\" : { \"0.1\" : { \"changes\": \"fix verified exception1\", \"downloadUrl\": \"https://search.maven.org/remotecontent?filepath=kg/apc/jmeter-plugins-webdriver/0.3/jmeter-plugins-webdriver-0.1.jar\" }," +
+                "\"0.2\" : { \"changes\": \"fix verified exception1\", \"downloadUrl\": \"https://search.maven.org/remotecontent?filepath=kg/apc/jmeter-plugins-webdriver/0.3/jmeter-plugins-webdriver-0.2.jar\"}," +
+                "\"0.3\" : { \"changes\": \"fix verified exception1\", " +
+                "\"downloadUrl\": \"https://search.maven.org/remotecontent?filepath=kg/apc/jmeter-plugins-webdriver/0.3/jmeter-plugins-webdriver-0.3.jar\"} }}";
         Plugin p = Plugin.fromJSON(JSONObject.fromObject(str, new JsonConfig()));
 
         Set<Plugin> set = new HashSet<>();
@@ -39,7 +49,9 @@ public class PluginsListTest {
 
         p.setCandidateVersion("0.3");
         assertEquals("0.3", pluginsList.getCbVersion(pluginsList.getCheckboxItem(p, null)));
-
+        String desc = pluginsList.getDescriptionHTML(p);
+        assertFalse(desc.indexOf(EXPECTED_DESC_MAVEN)<0);
+        assertFalse(desc.indexOf(EXPECTED_DESC_CHANGES)<0);
         p.detectInstalled(null);
         assertEquals(Plugin.VER_STOCK, pluginsList.getCbVersion(pluginsList.getCheckboxItem(p, null)));
 
