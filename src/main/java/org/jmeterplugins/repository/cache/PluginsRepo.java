351577f575a4ee68691c033da503f47ae65b7377
Artem Fedorov <artem.fedorov@blazemeter.com>
2018-05-18 18:44:34 +0300

    cache plugins repo (#37)
    * cache plugins repo
    
    * cleanup
    
    * md5
    
    * add RepoTests

---
+++ src/main/java/org/jmeterplugins/repository/cache/PluginsRepo.java
@@ -0,0 +1,78 @@
+package org.jmeterplugins.repository.cache;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.jorphan.logging.LoggingManager;
+import org.apache.log.Logger;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class PluginsRepo implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private static final Logger log = LoggingManager.getLoggerForClass();
+
+    private final String repoJSON;
+    private final long expirationTime;
+
+    public PluginsRepo(String repoJSON, long expirationTime) {
+        this.repoJSON = repoJSON;
+        this.expirationTime = expirationTime;
+    }
+
+    public boolean isActual() {
+        return expirationTime > System.currentTimeMillis();
+    }
+
+    public long getExpirationTime() {
+        return expirationTime;
+    }
+
+    public String getRepoJSON() {
+        return repoJSON;
+    }
+
+    public void saveToFile(File file) {
+        // Serialization
+        try {
+            FileUtils.touch(file);
+
+            //Saving of object in a file
+            FileOutputStream fout = new FileOutputStream(file);
+            ObjectOutputStream out = new ObjectOutputStream(fout);
+
+            // Method for serialization of object
+            out.writeObject(this);
+
+            out.close();
+            fout.close();
+
+        } catch (IOException ex) {
+            log.warn("Failed for serialize repo", ex);
+        }
+    }
+
+    public static PluginsRepo fromFile(File file) {
+        // Deserialization
+        try {
+            // Reading the object from a file
+            FileInputStream fis = new FileInputStream(file);
+            ObjectInputStream in = new ObjectInputStream(fis);
+
+            // Method for deserialization of object
+            PluginsRepo repo = (PluginsRepo) in.readObject();
+
+            in.close();
+            fis.close();
+
+            return repo;
+        } catch (IOException | ClassNotFoundException ex) {
+            log.warn("Failed for deserialize repo", ex);
+            return null;
+        }
+    }
+}
---
+++ src/test/java/org/jmeterplugins/repository/cache/PluginsRepoTest.java
@@ -0,0 +1,33 @@
+package org.jmeterplugins.repository.cache;
+
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.junit.Assert.*;
+
+public class PluginsRepoTest {
+
+    @Test
+    public void testFlow() throws Exception {
+        String serializedRepo = getClass().getResource("/serializedRepo").getFile();
+        File file = new File(serializedRepo);
+        PluginsRepo repo = PluginsRepo.fromFile(file);
+        assertNotNull(repo);
+        assertEquals(124810, repo.getRepoJSON().length());
+        assertEquals(1526736963000L, repo.getExpirationTime());
+        File tempFile = File.createTempFile("tmp_cache", "serialized");
+        repo.saveToFile(tempFile);
+        assertEquals(file.length(), tempFile.length());
+    }
+
+    @Test
+    public void testFlow2() throws Exception {
+        long l = System.currentTimeMillis();
+        PluginsRepo repo = new PluginsRepo("", l + 10000);
+        assertTrue(repo.isActual());
+
+        repo = new PluginsRepo("", l - 10000);
+        assertFalse(repo.isActual());
+    }
+}
