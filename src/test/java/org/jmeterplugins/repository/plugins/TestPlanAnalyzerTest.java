2def2a6580963f557e9b82931bb1d0a2318fb36b
Rithvik Patibandla <rithvikp98@gmail.com>
2023-08-10 13:55:46 +0530

    Add BackendListener impl class to nonExistentClasses while analyzing test plan (#67)
    * Add backend listener implementation class to non existent classes while analyzing test plan
    
    * Add test case
    
    ---------
    
    Co-authored-by: Rithvik Patibandla <rpatibandla@microsoft.com>

--- src/test/java/org/jmeterplugins/repository/plugins/TestPlanAnalyzerTest.java
+++ src/test/java/org/jmeterplugins/repository/plugins/TestPlanAnalyzerTest.java
@@ -28,4 +28,18 @@ public class TestPlanAnalyzerTest {
         assertTrue(classes.contains("kg.apc.jmeter.vizualizers.CorrectedResultCollector"));
         assertTrue(classes.contains("kg.apc.jmeter.samplers.DummySamplerGui"));
     }
+
+    @Test
+    public void testBackendListenerImpl() throws Exception {
+        String path = getClass().getResource("/testplan-with-backend-listener.xml").getPath();
+        TestPlanAnalyzer analyzer = new TestPlanAnalyzer();
+        Set<String> classes = analyzer.analyze(path);
+
+        assertEquals(5, classes.size());
+        assertTrue(classes.contains("kg.apc.jmeter.vizualizers.ResponseTimesOverTimeGui"));
+        assertTrue(classes.contains("kg.apc.jmeter.samplers.DummySampler"));
+        assertTrue(classes.contains("kg.apc.jmeter.vizualizers.CorrectedResultCollector"));
+        assertTrue(classes.contains("kg.apc.jmeter.samplers.DummySamplerGui"));
+        assertTrue(classes.contains("io.github.adrianmo.jmeter.backendlistener.azure.AzureBackendClient"));
+    }
 }
